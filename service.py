bba962c65c06b40892256e625f5831f4d52b4735
EugenNt <82192123+EugenNt@users.noreply.github.com>
2021-08-01 17:18:11 +0300

    Update service.py

--- service.py
+++ service.py
@@ -1,13 +1,32 @@
 
 
-class Service:
-    def __init__(self,id,name,money):
-        self.id=id
-        self.name=name
-        self.money=money
+lass Service:
+    def __init__(self,_id,_name,_price):
+        self.setId(_id)
+        self.setName(_name)
+        self.setPrice(_price)
     
+
+    def setId(self,id):
+        self._id=id
+    def getId(self):
+        return self._id
+
+    def setName(self,name):
+        self._name=name
+    def getName(self):
+        return self._name
+
+    def setPrice(self,price):
+        self._price=price
+    def getPrice(self):
+        return self._price
+
+
+
+
     def __str__(self):
-        return f"{self.id}-{self.name} {self.money}"
+        return f"{self._id}-{self._name} {self._price}"
 
     def __repr__(self):
         return str(self)
@@ -17,8 +36,8 @@ class ServiceRepositoryFactory:
     def __init__(self):
         self.lastCreatedId=0
         self.service=[]
-    def getService(self,name,money):
-        obj=Service(id,name,money)
+    def getService(self,name,price):
+        obj=Service(id,name,price)
         self.lastCreatedId+=1
         obj.id=self.lastCreatedId
         self.save(obj)
@@ -26,6 +45,8 @@ class ServiceRepositoryFactory:
 
     def save(self,service):
         self.service.append(service)
+    def saveAll(self,service):
+        self.sevice=service
     
     def all(self):
         return  tuple(self.service)
@@ -42,6 +63,8 @@ class ServiceRepositoryFactory:
     def deliteById(self,id):
         if self.id == id:
             self.service.pop(self.id -1 )
+
+      
 class Currency:
     def __init__(self,id,code,nominal,rate):
         self.id=id
@@ -50,10 +73,4 @@ class Currency:
         self.rate=rate
     
     def __repr__(self):
-        return f"id={self.id}\n code={self.code}\n nominal={self.nominal}\n rate={self.rate}"
-      
-
-
-srf=ServiceRepositoryFactory()
-s1=srf.getService("NameFirst",100)
-print(s1)
+        return f"\nid={self.id}\n code={self.code}\n nominal={self.nominal}\n rate={self.rate}"
